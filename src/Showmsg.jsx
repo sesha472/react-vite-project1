4ad40e6ec2b5f1de8ad09f1461b9a37837d7ce83
seshasai <sesha472@gmial.com>
2024-01-02 22:14:51 +0530

    commit

--- src/Showmsg.jsx
+++ src/Showmsg.jsx
@@ -3,14 +3,23 @@ import React ,{useState} from 'react';
 
 const Showmsg = () => {
     const [show, setshow] = useState(false);
+    const [username, setusername] = useState("")
 
 
   return (
+    <>
     <div>
         {show ? <p>all the best </p>:null}
     <button onClick={()=>setshow(!show)}>show message</button>
+    
       
     </div>
+    <div>
+      <p>{username}</p>
+      <input onChange={(event)=>setusername(event.target.value)} value={username}></input>
+    </div>
+
+    </>
   )
 }
 
